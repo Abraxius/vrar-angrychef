00e63b730756a710dd8d45dd306c73cdfe8c53dc
Juan Pablo Casillas <juan.pablo.cas.zer@gmail.com>
2024-06-18 14:04:16 +0200

    Bullet Tag

---
+++ Assets/Umgehen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Umgehen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+
+    }
+}
