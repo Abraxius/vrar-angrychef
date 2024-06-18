06bc897677616dc0092f254a42e42f720924ee8b
Alexander Jost <josti4a@t-online.de>
2024-06-18 12:45:21 +0200

    feat: customer queue worked now and customer walk to a seat

--- Assets/Scripts/Customers/CustomerController.cs
+++ Assets/Scripts/Customers/CustomerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
@@ -11,9 +12,12 @@ namespace AngryChief.Customer
     public class CustomerController : MonoBehaviour
     {
         public Transform m_Target;
+        public Vector3 m_Position;
         public int m_WaitingPosition;
         public int m_OldWaitingPosition;
 
+        [HideInInspector] public CustomerSpawnPoint m_CustomerSpawnManager;
+        
         private NavMeshAgent m_Agent;
 
         Animator m_Animator;
@@ -29,6 +33,8 @@ namespace AngryChief.Customer
         // Start is called before the first frame update
         void Start()
         {
+            m_Agent = GetComponent<NavMeshAgent>();
+            
             StartCoroutine(WalkToCashDesk()); //Nur zum testen
         }
 
@@ -49,19 +55,17 @@ namespace AngryChief.Customer
 
         public IEnumerator WalkToCashDesk()
         {
-            m_Agent = GetComponent<NavMeshAgent>();
-
             if (m_Target != null)
             {
                 m_Animator.SetBool("Walking", true);
-                m_Agent.SetDestination(m_Target.position);
+                m_Agent.SetDestination(m_Position);
             }
             else
             {
                 yield return null;
             }
 
-            // Warte bis der Agent tatsächlich am Ziel angekommen ist
+            // Warte bis der Agent tatsï¿½chlich am Ziel angekommen ist
             while (m_Agent.pathPending || m_Agent.remainingDistance > m_Agent.stoppingDistance || m_Agent.velocity.sqrMagnitude > 0f)
             {
                 yield return null;
@@ -85,20 +89,74 @@ namespace AngryChief.Customer
             }
 
         }
+        
+        public IEnumerator WalkToSeat()
+        {
+            if (m_Target != null)
+            {
+                m_Animator.SetBool("Walking", true);
+                m_Agent.SetDestination(m_Target.position);
+            }
+            else
+            {
+                yield return null;
+            }
 
+            // Warte bis der Agent tatsï¿½chlich am Ziel angekommen ist
+            while (m_Agent.pathPending || m_Agent.remainingDistance > m_Agent.stoppingDistance || m_Agent.velocity.sqrMagnitude > 0f)
+            {
+                yield return null;
+            }
+
+            m_Animator.SetBool("Walking", false);
+
+            m_Animator.SetTrigger("SitDown");
+            transform.rotation = m_Target.rotation;
+            
+            Debug.Log("ERSTMAL WAMPE VOLLHAUFEN!!!!");
+
+        }
+        
         public void FinishOrder()
         {
+            //Start walking to the seat
+            foreach (var seat in m_CustomerSpawnManager.m_SeatList)
+            {
+                if (!seat.m_Busy)
+                {
+                    m_Target = seat.m_Transform;
+                    seat.m_Busy = true;
+                    break;
+                }
+            }
+
+            StartCoroutine(WalkToSeat());
             GameManager.Instance.m_CustomersList.Remove(this);
+            GameManager.Instance.m_CurrentWaitingCustomer -= 1;
+            
+            //Start movement from waiting customers
             foreach (var customer in GameManager.Instance.m_CustomersList)
             {
-                customer.m_Target.position = m_Target.position + new Vector3(0, 0, +2f);
+                //customer.m_Target.position = m_Target.position + new Vector3(0, 0, +2f);
+                //customer.m_OldWaitingPosition = customer.m_WaitingPosition;
+                customer.m_WaitingPosition -= 1;
+                
+                customer.m_Position = customer.m_Target.position + new Vector3(0, 0, -customer.m_WaitingPosition * 1.5f);
+                Debug.Log(" Individuelle Warteposition " + customer.m_WaitingPosition  +" Neue Position: " + customer.m_Position);
                 customer.StartWalk();
             }
 
-            GameManager.Instance.m_CurrentWaitingCustomer -= 1;
-            Destroy(gameObject);
+
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Collider>().tag == "Seat")
+            {
+                Debug.Log("SITZ ERREICHT BOOOM");
+                //m_Animator.SetBool("Sit", true);
+            }
+        }
     }
 
 }
