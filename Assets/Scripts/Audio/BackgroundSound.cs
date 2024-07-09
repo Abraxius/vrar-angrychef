4e106712c4a523a31c145c3f203915bbe72a12ec
Felix Stumpf <139617443+felixStumpf@users.noreply.github.com>
2024-07-09 13:21:14 +0200

    Add ambient music for normal mode

--- Assets/Scripts/Audio/BackgroundSound.cs
+++ Assets/Scripts/Audio/BackgroundSound.cs
@@ -8,7 +8,14 @@ public class BackgroundSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AudioManager.Instance.Play("mcdonalds");
+        if (GameManager.Instance.m_FunLevel)
+        {
+            AudioManager.Instance.Play("background_mcdonalds");
+        }
+        else
+        {
+            AudioManager.Instance.Play("background_jazz");
+        }
     }
 
     // Update is called once per frame
