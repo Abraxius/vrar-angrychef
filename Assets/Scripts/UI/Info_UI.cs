3b9d460cddcf073433157a71972a9962ecafc913
Juan Pablo Casillas <juan.pablo.cas.zer@gmail.com>
2024-09-12 14:33:43 +0200

    Neu UI im GameScene.

---
+++ Assets/Scripts/UI/Info_UI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info_UI : MonoBehaviour
+{
+    [SerializeField] Text text;
+    public GameManager gameManager;
+
+    public enum Stats { Health, Score, Money, Diamands }
+    public Stats statToUpdate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        switch (statToUpdate)
+        {
+            case Stats.Money:
+                UpdateText(gameManager.m_Money);
+                break;
+            /*case Stats.Score:
+                UpdateText(gameManager.m_Score);
+                break;*/
+            case Stats.Health:
+                UpdateText(gameManager.m_Life);
+                break;
+            case Stats.Diamands:
+                UpdateText(gameManager.m_Diamands);
+                break;
+            default:
+                Debug.Log("Error stat not defined");
+                break;
+        }
+    }
+
+    private void UpdateText(float value)
+    {
+        text.text = value.ToString();
+    }
+}
+
