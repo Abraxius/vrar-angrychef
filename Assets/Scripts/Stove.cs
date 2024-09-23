f8f2ba2ebcb500a67fa1444cbd3c95c34129e553
Tim Liehr <67097570+BrI4n99@users.noreply.github.com>
2024-09-23 22:25:49 +0200

    Cooking updated

--- Assets/Scripts/Stove.cs
+++ Assets/Scripts/Stove.cs
@@ -7,6 +7,7 @@ public class Stove : IngredientSnapping
     [SerializeField] private float cookTime = 5f; // Time to change from uncooked to cooked
     [SerializeField] private float burnTime = 10f; // Time to change from cooked to burned
     private float timer = 0f;
+    private GameObject snappedBurger;
 
     // Update is called once per frame
     void Update()
@@ -30,12 +31,56 @@ public class Stove : IngredientSnapping
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Snappable")
+        if (other.gameObject.tag == "Snappable" && !snapped)
         {
             if (other.gameObject.GetComponent<Ingredient>().fryable && ((snappedIngredients.Ingredients.Count == 0 && !other.gameObject.GetComponent<Ingredient>().isSnapped) || (snappedIngredients.Ingredients.Count >= 1 && stackable == true && !other.gameObject.GetComponent<Ingredient>().isSnapped)))
             {
                 SnapObject(other.gameObject);
+                snappedBurger = other.gameObject;
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exit");
+        if (other.gameObject.Equals(snappedBurger) && snapped)
+        {
+            TakeObject(other.gameObject);
+            Debug.Log("Burger aus der Pfanne genommen");
+        }
+        else
+        {
+            Debug.Log("Nicht der aktuelle Burger");
+        }
+    }
+
+    private void TakeObject(GameObject snappedObject)
+    {
+        // Prüfe, ob das Objekt überhaupt einen Parent hat
+        if (snappedObject.transform.parent != null)
+        {
+            Debug.Log("Parent before: " + snappedObject.transform.parent.name);
+        }
+        else
+        {
+            Debug.Log("Das Objekt hat keinen Parent.");
+        }
+
+        snapped = false;
+        snappedBurger = null;
+        Ingredient ingredient = snappedObject.GetComponent<Ingredient>();
+        ingredient.isSnapped = false;
+        snappedObject.transform.parent = null;
+        snappedIngredients.Ingredients.Remove(ingredient);
+
+
+        // Deactivate Rigidbody/physics
+        Rigidbody rb = snappedObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.useGravity = true;
+            rb.isKinematic = false;
         }
     }
 }
