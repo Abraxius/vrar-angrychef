69b4f4db8a1b294dd6a797822f2fb79bc2df313c
Juan Pablo Casillas <juan.pablo.cas.zer@gmail.com>
2024-09-25 22:11:14 +0200

    Modified old shop to only have money Upgrades

---
+++ Assets/Scripts/Upgrades/Scripts/IngredientsUpgrade.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IngredientsUpgrade", menuName = "Upgrades/Ingredients")]
+public class IngredientsUpgrade : BaseUpgrade
+{
+    public override void Apply()
+    {
+        
+    }
+}
